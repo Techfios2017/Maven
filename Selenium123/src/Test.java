6a96953aa54c59e15fde7389b37d87d3453497ad
Techfios2017 <30378494+Techfios2017@users.noreply.github.com>
2017-09-10 09:54:36 -0400

    added test suite runner

---
+++ Selenium123/src/Test.java
@@ -0,0 +1,13 @@
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Result result=JUnitCore.runClasses(TestSuitExample.class);
+	
+	
+	}
+
+}
